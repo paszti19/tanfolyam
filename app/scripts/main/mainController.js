340f226ff2d9a3604be18b5f9cf9f7204cae1532
hajdu.gabor <hajdu.gabor@danubiusinfo.hu>
2017-03-14 12:09:28 +0100

    language selector

--- app/scripts/main/mainController.js
+++ app/scripts/main/mainController.js
@@ -1,10 +1,12 @@
 app.controller('mainController', MainController);
 
-MainController.$inject = ['$timeout'];
+MainController.$inject = ['$translate', '$scope', '$timeout'];
 
 
-function MainController($timeout) {
+function MainController($translate, $scope, $timeout) {
   var vm = this;
+
+  vm.langOptions = [];
 
   vm.menuItems = [
     {
@@ -23,6 +25,33 @@ function MainController($timeout) {
       state: 'list'
     }
   ];
+
+  switchLang();
+
+  function switchLang() {
+    $translate('LANG.HU').then(function () {
+      vm.langOptions = [
+        {
+          text: $translate.instant('LANG.HU'),
+          value: 'hu'
+        },
+        {
+          text: $translate.instant('LANG.EN'),
+          value: 'en'
+        }
+      ];
+    });
+  }
+
+  vm.lang = 'hu';
+
+  $scope.$on('$select.select', function (event, value, index, elem) {
+    $translate.use(value).then(function () {
+      switchLang();
+    });
+
+  });
+
   //
   // $timeout(() => {
   //   vm.menuItems.push({
